a862f83367f94ac8ddcabcf3785e37f8ad3cb7fc
buithien <thien7278@gmail.com>
2023-02-16 00:03:14 +0700

    design form login  pass one

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+import "./Login.scss";
+import { FormattedMessage } from "react-intl";
+import { Label } from "reactstrap";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.btnLogin = React.createRef();
+  }
+  render() {
+    return (
+      <div className="login-backgroud">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 text-login">Login</div>
+            <div className="col-12 form-group login-input">
+              <Label>Username:</Label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="col-12 form-group login-input">
+              <Label>Password:</Label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="col-12">
+              <button className="btn-login">Login</button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password">Forgot your password?</span>
+            </div>
+            <div className="col-12 text-center mt-3">
+              <span className="text-other-login">Or Login with: </span>
+            </div>
+            <div className="col-12 social-login">
+              <i className="fab fa-brands fa-google-plus-g google"></i>
+              <i className="fab fa-brands fa-facebook-f facebook"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+    adminLoginSuccess: (adminInfo) =>
+      dispatch(actions.adminLoginSuccess(adminInfo)),
+    adminLoginFail: () => dispatch(actions.adminLoginFail()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
