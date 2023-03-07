256b1f75051cf2031feb0ed45bed2a760bfeea37
buithien <thien7278@gmail.com>
2023-03-07 22:35:59 +0700

    Reactjs Slider carousel#48

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,7 @@ import Header from './Header/Header';
 import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
-
+import CustomScrollbars from '../components/CustomScrollbars';
 class App extends Component {
 
     handlePersistorState = () => {
@@ -47,14 +47,16 @@ class App extends Component {
                     <div className="main-container">
                         {this.props.isLoggedIn && <Header />}
 
-                        <span className="content-container">
+                        <div className="content-container">
+                        <CustomScrollbars style={{height:'100vh',with:'100%'}}>
                             <Switch>
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
                                 <Route path={path.HOMEPAGE} component={HomePage} />
                             </Switch>
-                        </span>
+                        </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
-
+import Specialty from './Section/Specialty';
 class HomePage extends Component {
 
     render() {
       
-        return (
+        return ( 
            <div>
             <HomeHeader></HomeHeader>
+            <Specialty></Specialty>
+            <div style={{height:'300px'}}></div>
            </div>
         );
     }
